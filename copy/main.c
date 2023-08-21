d9d7c8d753590dc89fcfe035c10800c37ff8e1f1
anna.maniuk <anna.maniuk@accenture.com>
2023-08-21 15:05:46 -0400

    Initial Commit

---
+++ copy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  copy
+//
+//  Created by anna.maniuk on 8/21/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
